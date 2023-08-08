af016d7866801fd90615fbbe173ce3f13bb6289c
hyunsik jung <ball1241@naver.com>
2023-08-08 11:32:03 +0900

    자기소개 마무리

--- app/src/main/java/com/example/introduce/HomeActivity.kt
+++ app/src/main/java/com/example/introduce/HomeActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var textViewUsername: TextView
+    private lateinit var textViewInfo: TextView // 이름, 나이, MBTI 정보를 표시할 TextView
     private lateinit var buttonExit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.home_layout)
 
         textViewUsername = findViewById(R.id.textViewUsername)
+        textViewInfo = findViewById(R.id.textViewInfo) // XML 레이아웃에 추가한 TextView 연결
         buttonExit = findViewById(R.id.buttonExit)
 
         val intent = intent
@@ -24,6 +26,13 @@ class HomeActivity : AppCompatActivity() {
         username?.let {
             textViewUsername.text = "$username 님,"
         }
+
+        // 추가된 부분: 이름, 나이, MBTI 정보 설정 및 표시
+        val name = "정현식"
+        val age = "27"
+        val mbti = "ISTP"
+        val infoText = "이름: $name\n나이: $age\nMBTI: $mbti"
+        textViewInfo.text = infoText
 
         buttonExit.setOnClickListener {
             val signInIntent = Intent(this@HomeActivity, SignInActivity::class.java)
